3e02d983cd6a8ccf48a58a5e350058f3ed590b58
Jroslav Trofimov <Jroslav1985@gmail.com>
2024-02-25 17:10:11 +0500

    added first commit

---
+++ src/arraylist/Main.java
@@ -0,0 +1,31 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+	private static final String TEXT = "It looks like it's going to rain soon.";
+
+	public static void main(String[] args) {
+
+		char[] characters = TEXT.toCharArray();
+		List<Character> chList = new ArrayList<>();
+		chList = addList(characters);
+		System.out.println(chList);
+		
+		int i = Collections.frequency(chList, 'o');
+		System.out.println(i);
+	}
+
+	private static List<Character> addList(char[] characters) {
+		List<Character> list = new ArrayList<>();
+		for (int i = 0; i < characters.length; i++) {
+			if (!(characters[i] + "").matches("[\\p{Punct}\\s]+")) {
+				list.add(characters[i]);
+			}
+		}
+		return list;
+	}
+}
